bf93bb13b773c12d60810813826536e671bf7b32
KingTingTheGreat <jting@bu.edu>
2024-02-15 23:11:12 -0500

    initial commit

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/jting", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello from JTing")
+	})
+}
